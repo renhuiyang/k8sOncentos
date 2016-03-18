8e2e884f5217b63a6e520f0a24276c5ed7d6a434
renhui.yang <renhui.yang2008@gmail.com>
2016-03-18 11:18:42 +0800

    update master.sh

--- Master/centos7-master.sh
+++ Master/centos7-master.sh
@@ -4,26 +4,19 @@ yum -y update
 systemctl disable iptables-services firewalld
 systemctl stop iptables-services firewalld
 
-yum -y remove docker-selinux-1.8.2-10.el7.centos.x86_64
-yum -y remove docker-1.8.2-10.el7.centos.x86_64
 
-yum install -y etcd flannel kubernetes-master 
+tee /etc/yum.repos.d/k8s.repo <<-'EOF'
+[virt7-docker-common-release]
+name=virt7-docker-common-release
+baseurl=http://cbs.centos.org/repos/virt7-docker-common-release/x86_64/os/
+gpgcheck=0
+EOF
 
-#tee /etc/yum.repos.d/docker.repo <<-'EOF'
-#[dockerrepo]
-#name=Docker Repository
-#baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
-#enabled=1
-#gpgcheck=1
-#gpgkey=https://yum.dockerproject.org/gpg
-#EOF
-#yum -y install docker-engine
-
-#systemctl stop docker
-#rm -rf /var/lib/docker
+yum install -y etcd flannel
+yum -y install --enablerepo=virt7-docker-common-release kubernetes
 
 sed -i 's/ETCD_LISTEN_CLIENT_URLS=.*/ETCD_LISTEN_CLIENT_URLS="http:\/\/0.0.0.0:2379"/g' /etc/etcd/etcd.conf
-sed -i 's/ETCD_LISTEN_PEER_URLS=.*/ETCD_LISTEN_PEER_URLS="http://localhost:2380"/g' /etc/etcd/etcd.conf
+sed -i 's/#ETCD_LISTEN_PEER_URLS=.*/ETCD_LISTEN_PEER_URLS="http://localhost:2380"/g' /etc/etcd/etcd.conf
 sed -i 's/ETCD_ADVERTISE_CLIENT_URLS=.*/ETCD_ADVERTISE_CLIENT_URLS="http://0.0.0.0:2379"/g' /etc/etcd/etcd.conf
 
 sed -i 's/KUBE_MASTER=.*/KUBE_MASTER="--master=http:\/\/0.0.0.0:8080"/g' /etc/kubernetes/config
@@ -42,20 +35,19 @@ systemctl restart etcd
 systemctl enable etcd
 systemctl status etcd
 
+sleep 10s
+
 etcdctl mk /k8s.io/network/config '{"Network":"172.17.0.0/16"}'
+
+sleep 10s
 
 systemctl restart flanneld
 systemctl enable flanneld
 systemctl status flanneld
 
-for SERVICES in docker kube-apiserver kube-controller-manager kube-scheduler; do 
+for SERVICES in docker kube-apiserver kube-controller-manager kube-scheduler; do
     systemctl restart $SERVICES
     systemctl enable $SERVICES
-    systemctl status $SERVICES 
+    systemctl status $SERVICES
 done
-
- 
-
-#sed -i 's/FLANNEL_ETCD=.*/FLANNEL_ETCD="http:\/\/localhost:2379"/g' /etc/sysconfig/flanneld
-#sed -i 's/FLANNEL_ETCD_KEY=.*/FLANNEL_ETCD_KEY="\/k8s.io\/network"/g' /etc/sysconfig/flanneld
 
